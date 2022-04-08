230951da833e857e8a12f7c839623cd16795fcd5
Nátalia <nataliaefernanda3054@gmail.com>
2022-04-08 10:31:51 -0300

    commit pra duda

--- Assets/database/database.js
+++ Assets/database/database.js
@@ -1,3 +1,8 @@
 const questions = [
-    
+    "Para o presente contínuo a terminação -ing ao final do verbo significa o nosso ando, endo, indo, ou seja, o gerúndio na língua portuguesa.",
+    1- "Verbos terminados em CVC (consoante + vogal + consoante) repete a ultima Letra, Exemplos: Run = Running",
+    2- "Verbos terminados em E, tiramos o E e colocamos ING, Exemplos: Ride = Riding",
+    3- "Verbos terminados em IE, trocamos o IE po Y e colocamos ING, Exemplos: Lie = Lying",
+    4- "Verbos terminado em CVC com som forte na ultima silaba NAO REPETE A ULTIMA LETRA, Exemplos: Open = Opening",
+    5- "Verbos terminado  em EE nao tiramos E, mas adicionamos o ING, Exemplos: See = Seeing",
 ]
